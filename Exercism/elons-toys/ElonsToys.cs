f35851cbf9cc4489265b24a613af6cfbea195dde
kobubu <79473433+kobubu@users.noreply.github.com>
2024-09-10 13:16:01 +0300

    Add files via upload

---
+++ Exercism/elons-toys/ElonsToys.cs
@@ -0,0 +1,31 @@
+using System;
+class RemoteControlCar
+{
+    int _charge;
+    int _distance;
+
+    public RemoteControlCar()
+    {
+        _charge = 100;
+        _distance = 0;
+    }
+    public static RemoteControlCar Buy() => new();
+
+    //method to return the distance as displayed on the LED display:
+    public string DistanceDisplay()
+    {
+        return $"Driven {_distance} meters";
+    }
+
+    public string BatteryDisplay() =>
+    _charge == 0 ? "Battery empty" : $"Battery at {_charge}%";
+
+    public void Drive()
+    {
+        if (_charge <= 0) return;
+        _charge--;
+        _distance += 20;
+    }
+}
+
+
