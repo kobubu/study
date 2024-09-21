ab40ecf57469e9baefb98de3f1e5bd0cc8f1c1dd
kobubu <79473433+kobubu@users.noreply.github.com>
2024-09-21 23:51:38 +0300

    Update 17. Between_min_max_lambda.py

--- Python/Skillfactory/Python basic/Module 3. Introduction to functions/4. Default arguments/8 Final/17. Between_min_max_lambda.py
+++ Python/Skillfactory/Python basic/Module 3. Introduction to functions/4. Default arguments/8 Final/17. Between_min_max_lambda.py
@@ -3,6 +3,6 @@
  Функция принимает на вход числа через запятую и возвращает одно число — среднее между максимумом и минимумом этих чисел.
 '''
 
-between_min_max = lambda *args: sum(args)/len(args)
+between_min_max = lambda *args: (max(args) + min(args))/2
 
 print(between_min_max(2,3,4,5,6,7,8,9))
