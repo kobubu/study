270d77f8d3109103c68245cf2ba157f9434bb161
kobubu <79473433+kobubu@users.noreply.github.com>
2024-09-26 14:19:38 +0300

    Create Ex. 2.5. Count characters.py

---
+++ Python/Skillfactory/Python basic/Hw2/Python basics/Ex. 2.5. Count characters.py
@@ -0,0 +1,17 @@
+'''
+Дана строка python_string.
+
+Посчитайте количество символов в этой строке и возведите получившееся число в третью степень. Результат запишите в переменную result.
+
+Примеры работы программы:
+
+
+python_string = 'Hello! My name is Python. I will help you to analyze some data.'
+## result = 250047
+
+python_string = 'Привет! Меня зовут Питон. Я помогу вам проанализировать некоторые данные.'
+## result = 389017
+
+'''
+python_string = 'Hello! My name is Python. I will help you to analyze some data.'
+result = len(python_string)**3
