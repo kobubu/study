65f1c3c86a1bd21124ace7cf735f874097123a4b
kobubu <79473433+kobubu@users.noreply.github.com>
2024-10-04 10:56:26 +0300

    Add files via upload

---
+++ Exercism/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
@@ -0,0 +1,36 @@
+using System;
+
+public static class CentralBank
+{
+    public static string DisplayDenomination(long @base, long multiplier)
+    {
+        try 
+        {
+        return checked(@base * multiplier).ToString();
+        }
+        catch 
+        {
+        return "*** Too Big ***";
+        }
+    }
+
+    public static string DisplayGDP(float @base, float multiplier)
+    {
+        var product = @base * multiplier;
+        
+            return product != float.PositiveInfinity ? product.ToString() : "*** Too Big ***";
+        
+    }
+
+    public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
+    {
+        try
+        {
+            return checked(@salaryBase * multiplier).ToString();
+        }
+        catch (OverflowException e)
+        {
+            return "*** Much Too Big ***";
+        }
+    }
+}
