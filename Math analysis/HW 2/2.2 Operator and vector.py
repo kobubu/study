2c77dd96918173eae59eb210f370d589d71532be
kobubu <79473433+kobubu@users.noreply.github.com>
2024-11-27 18:04:34 +0300

    Update 2.2 Operator and vector.py

--- Math analysis/HW 2/2.2 Operator and vector.py
+++ Math analysis/HW 2/2.2 Operator and vector.py
@@ -5,10 +5,21 @@ a) Найдите образ вектора v [1, 2, 3] при действии 
 v =
 '''
 
-
 import numpy as np
 
-v = np.array([1, 2, 3])
-F = np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
+# Define the vector v as a column vector
+v = np.array([[1],
+              [2],
+              [3]])
 
-print(v@F)
+# Define the operator F as a 2D array
+F = np.array([[1, 0, 0],
+              [0, 0, 1],
+              [0, -1, 0]])
+
+# Compute the image of v under F
+image_v = F @ v
+
+# Print the result
+print("The image of v under F is:")
+print(image_v)
