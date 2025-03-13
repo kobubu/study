429dd4025920d82c17a607776243d3a170e1ed57
kobubu <79473433+kobubu@users.noreply.github.com>
2025-03-14 00:28:14 +0300

    Update client.py

--- Ml_ops/client.py
+++ Ml_ops/client.py
@@ -1,12 +1,18 @@
 import requests
+import numpy as np
 
 if __name__ == '__main__':
-    r = requests.post('http://localhost:5001/add', json={'num': 15})
+    a = [1, 2, 3, 4]
+    vector = np.array(a)
+
+    # Преобразуем numpy.ndarray в список
+    vector_list = vector.tolist()
+
+    # Отправляем POST-запрос с JSON-телом
+    r = requests.post('http://localhost:5001/predict', json={'features': vector_list})
+
     print(r.status_code)
     if r.status_code == 200:
-        print(r.json()['result'])
+        print(r.json()['prediction'])
     else:
         print(r.text)
-
-
-
